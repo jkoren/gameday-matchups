516ba8ab3133deabc112a477f6b28bde48764bdc
jkoren <j.korenstein@gmail.com>
2021-03-10 17:23:11 -0500

    set up offense with Patriots

---
+++ README2.md
@@ -0,0 +1,34 @@
+
+# GAMEDAY MATCHUPS
+
+Gameday Matchups is a responsive web application to show American football fans the individual player matchups between 2 teams.
+
+[Run the deployed application](https://yard-service.herokuapp.com/)
+
+## Developer
+- JK
+
+## Built with
+- [React.js](https://reactjs.org/docs/getting-started.html)
+
+## Run Locally
+The setup steps expect the following tools/versions:
+- NodeJS
+
+###### Clone the Repo
+```
+git clone 
+```
+
+###### Install Dependencies
+```
+yarn install 
+bundle install 
+```
+
+```
+###### Start the webpack-dev-server
+```
+yarn start
+```
+###### The application can be accessed locally at <http://localhost:3000>
--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const reactContentRoot = document.getElementById("root")
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+  return (
+    <div className="App">
+      <div className="container">
+        <img src="https://jkorenstein-production.s3.amazonaws.com/football-gameday-player-matchups/American_Football_Positions_no_names.png" alt="Football" width="70%"/> 
+        {/* <img src="https://jkorenstein-production.s3.amazonaws.com/football-gameday-player-matchups/American_Football_Positions.jpg" alt="Football" width="70%"/>  */}
+        <div className="team"><b>New England</b></div>
+        <div className="quarterback">Cam Newton/QB</div>
+        <div className="fullback">Damien Harris/RB</div>
+        <div className="halfback">James White/RB</div>
+        <div className="leftwidereceiver">N'Keal Harry/WR</div>
+        <div className="rightwidereceiver">Damiere Byrd/WR</div>
+        <div className="lefttackle">Jermain Eluemunor/LT</div>
+        <div className="leftguard">Joe Thuney/LG</div>
+        <div className="center">David Andrews/C</div>
+        <div className="rightguard">Shaq Mason/RG</div>
+        <div className="righttackle">Mike Onwenu/RT</div>
+        <div className="tightend">Devin Asiasi/TE</div>
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, reactContentRoot)
